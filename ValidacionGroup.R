6f6a726b3e3b0c75ae16e3b4e1bc259ba78274b1
William Mendez <mate282010@hotmail.com>
2023-04-13 15:56:43 -0500

    metodo1

--- ValidacionGroup.R
+++ ValidacionGroup.R
@@ -15,12 +15,42 @@ datos3
 datos4<-dataWine[20:22,2:5]
 datos4
 fulldatos<-rbind(datos,datos2,datos3,datos4)
+metodo2<-c(1,1,1,1,2,2,2,2,2,2,2,2)
+metodo1<-c(1,1,1,1,1,2,2,2,2,1,1,1)
+fulldatos<-cbind(fulldatos,metodo1,metodo2)
 fulldatos
-media1<-mean(datos[,1])+mean(datos4[,1])
-media1
-media2<-mean(datos[,2])+mean(datos4[,2])
-media2
-media3<-mean(datos[,3])+mean(datos4[,3])
-media3
-media4<-mean(datos[,4])+mean(datos4[,4])
-media4
+grupo1<-which(metodo1 %in% 1)
+grupo2<-which(metodo1 %in% 2)
+grupo1
+grupo2
+mediaMetodo1_g1<-apply(fulldatos[grupo1,],2, mean)
+mediaMetodo1_g1
+mediaMetodo1_g2<-apply(fulldatos[grupo2,],2, mean)
+mediaMetodo1_g2
+grupo1m2<-which(metodo2 %in% 1)
+grupo2m2<-which(metodo2 %in% 2)
+grupo1m2
+grupo2m2
+mediaMetodo2_g1<-apply(fulldatos[grupo1m2,],2, mean)
+mediaMetodo2_g1
+mediaMetodo2_g2<-apply(fulldatos[grupo2m2,],2, mean)
+mediaMetodo2_g2
+
+
+euclidiana <- function(a,b) (sqrt ( sum ((a - b) ^ 2)))
+
+
+distancia1m1g1<-sapply(grupo1,function(x) euclidiana(mediaMetodo1_g1,fulldatos[x,1:4]))
+distancia1m1g1
+distancia1m1g2<-sapply(grupo2,function(x) euclidiana(mediaMetodo1_g2,fulldatos[x,1:4]))
+distancia1m1g2
+
+distancia1m2g1<-sapply(grupo1m2,function(x) euclidiana(mediaMetodo2_g1,fulldatos[x,1:4]))
+distancia1m2g1
+distancia1m2g2<-sapply(grupo2m2,function(x) euclidiana(mediaMetodo2_g2,fulldatos[x,1:4]))
+distancia1m2g2
+
+sumad1m1<-sum(distancia1m1g1**2)+sum(distancia1m1g2**2)
+sumad1m1
+sumad1m2<-sum(distancia1m2g1**2)+sum(distancia1m2g2**2)
+sumad1m2
