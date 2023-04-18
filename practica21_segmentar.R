9767fa3a247fd1aa4ba6876d2d9d926ac76fd15d
William Mendez <mate282010@hotmail.com>
2023-04-18 15:50:12 -0500

    interando k fuzzy medias

--- practica21_segmentar.R
+++ practica21_segmentar.R
@@ -64,7 +64,8 @@ medias2<-c((sumas2[1]/sum(PBb2)),
 medias3<-c((sumas3[1]/sum(PBb3)),
            (sumas3[2]/sum(PBb3)),
            (sumas3[3]/sum(PBb3)))
-dim<-length(medias1)
+dim<-dim(dataPixels[1])
+dim
 euclidiana <- function(a,b) (sqrt ( sum ((a - b) ^ 2)))
 
 
@@ -97,4 +98,66 @@ pertenencia2 <- (1/distancia2**2)**(1/(1.5-1))/sumadistancias
 pertenencia2
 pertenencia3 <- (1/distancia3**2)**(1/(1.5-1))/sumadistancias
 pertenencia3
-
+medias1
+epsilon<-0.1
+medias1
+medias2
+medias3
+difmedias<-1
+while (difmedias > epsilon) {
+  
+  #beta<-1/(pertenencia1+pertenencia2+pertenencia3)
+  
+  #PBb1<-(pertenencia1*beta)**1.5
+  #PBb2<-(pertenencia2*beta)**1.5
+  #PBb3<-(pertenencia3*beta)**1.5
+  PBb1<-(pertenencia1)**1.5
+  PBb2<-(pertenencia2)**1.5
+  PBb3<-(pertenencia3)**1.5
+  
+  VectorPEscalar1<-dataPixels[,]*PBb1
+  VectorPEscalar2<-dataPixels[,]*PBb2
+  VectorPEscalar3<-dataPixels[,]*PBb3
+  
+  sumas1<-c((sum(VectorPEscalar1[,1])),
+            (sum(VectorPEscalar1[,2])),
+            (sum(VectorPEscalar1[,3])))
+  sumas2<-c((sum(VectorPEscalar2[,1])),
+            (sum(VectorPEscalar2[,2])),
+            (sum(VectorPEscalar2[,3])))
+  sumas3<-c((sum(VectorPEscalar3[,1])),
+            (sum(VectorPEscalar3[,2])),
+            (sum(VectorPEscalar3[,3])))
+  
+  mediasact1<-c((sumas1[1]/sum(PBb1)),
+             (sumas1[2]/sum(PBb1)),
+             (sumas1[3]/sum(PBb1)))
+  mediasact2<-c((sumas2[1]/sum(PBb2)),
+             (sumas2[2]/sum(PBb2)),
+             (sumas2[3]/sum(PBb2)))
+  mediasact3<-c((sumas3[1]/sum(PBb3)),
+             (sumas3[2]/sum(PBb3)),
+             (sumas3[3]/sum(PBb3)))
+  
+  distancia1<-sapply(seq(1:dim),function(x) euclidiana(mediasact1,dataPixels[x,]))
+  distancia2<-sapply(seq(1:dim),function(x) euclidiana(mediasact2,dataPixels[x,]))
+  distancia3<-sapply(seq(1:dim),function(x) euclidiana(mediasact3,dataPixels[x,]))
+  
+  sumadistancias<-(((1/distancia1**2))**(1/(1.5-1)))+(((1/distancia2**2))**(1/(1.5-1))+((1/distancia3**2))**(1/(1.5-1)))
+  
+  pertenencia1 <- (1/distancia1**2)**(1/(1.5-1))/sumadistancias
+  pertenencia2 <- (1/distancia2**2)**(1/(1.5-1))/sumadistancias
+  pertenencia3 <- (1/distancia3**2)**(1/(1.5-1))/sumadistancias
+  
+  dif1<-((sum(medias1-mediasact1)**2))**(1/2)
+  dif2<-((sum(medias2-mediasact3)**2))**(1/2)
+  dif3<-((sum(medias3-mediasact3)**2))**(1/2)
+  medias1<-mediasact1
+  medias2<-mediasact2
+  medias3<-mediasact3
+  difmedias<-(sum(dif1,dif2,dif3))/3
+}
+difmedias
+medias1 
+medias2
+medias3
