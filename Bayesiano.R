ad4ed6c806d0724481dc5a71c576177649c35ee9
William Mendez <mate282010@hotmail.com>
2023-05-31 10:47:35 -0500

    kfold

--- Bayesiano.R
+++ Bayesiano.R
@@ -20,6 +20,8 @@ summary(dataFungiF)
 Prob_clase<-table(dataFungiF$clase)
 Prob_clase <- Prob_clase/dim(dataFungiF)[1]
 
+
+
 Prob_clase_E_VeilColor<-table(dataFungiF$clase,dataFungiF$`veil-color`)
 sumRows_Prob_clase_E_VeilColor<-rowSums(Prob_clase_E_VeilColor)
 sumCols_Prob_clase_E_VeilColor<-colSums(Prob_clase_E_VeilColor)
@@ -42,7 +44,8 @@ sumCols_Prob_clase_E_Habi<-colSums(Prob_clase_E_Habi)
 
 datafungiTrain <- dataFungiF[1:6000,]
 dataFungiTest <- dataFungiF[6001:8124,]
-
+Prob_clase<-table(datafungiTrain$clase)
+Prob_clase <- Prob_clase/dim(datafungiTrain)[1]
 
 #Verosimilitud-indep
 verIndFC <- function(dataClase,dataFeat,namesFC){
@@ -66,7 +69,7 @@ verosimiFeatFeat_RT <- verosimiFeatClase[["ring-type"]]
 verosimiFeatFeat_P <- verosimiFeatClase[["population"]]
 verosimiFeatFeat_H <- verosimiFeatClase[["habitat"]]
 
-obj1 <- dataFungiTest[1,][["population"]]
+obj1 <- dataFungiTest[1,]
 obj1
 res1 <- verosimiFeatFeat_VC["e",obj1[["veil-color"]]]
 valClases <- levels(dataFungiF$clase)
@@ -80,6 +83,8 @@ verosimTotprod*Prob_clase[1]
 #p(clase=e)=p(clase=e)p(v=w,r=e,p=v,h=p|clase=e)
 Resultado_test <- Prob_clase[1]*verosimiFeatFeat_VC["e","w"]*verosimiFeatFeat_RT["e","e"]*verosimiFeatFeat_P["e","v"]*verosimiFeatFeat_H["e","p"]
 indObj <- c(1:dim(dataFungiTest)[1])
+
+
 Result_Test<-sapply(indObj,function(x) sapply(namesFeatures,function(y) verosimiFeatClase[[y]][valClases[1],dataFungiTest[x,][[y]]]))
 dim(Result_Test)
 Result_Test <- t(Result_Test)
@@ -91,7 +96,27 @@ Result_Test2 <- t(Result_Test2)
 Result_Test2_prod <- sapply(c(1:dim(dataFungiTest)[1]), function(X) prod(Result_Test2[X,])*Prob_clase[2])
 
 
+valClases <- levels(dataFungiF$clase)
+res1 <- sapply(namesFeatures, function(x) verosimiFeatClase[[x]][valClases[1],obj1[[x]]])
+res1
+vero_obj1 <- prod(res1)
+vero_obj1
+nrows <- nrow(dataFungiTest)
+print(nrows)
+Result_Test <- sapply(seq(1:nrows), function(y) sapply(namesFeatures, function(x) verosimiFeatClase[[x]][valClases[1],dataFungiTest[y,][[x]]]))
+Result_Test <- t(Result_Test)
+Result_Test_prod <- sapply(seq(1:nrows), function(x) prod(Result_Test[x,])*Prob_clase[1])
+Result_Test_prod
+
+Result_Test2 <- sapply(seq(1:nrows), function(y) sapply(namesFeatures, function(x) verosimiFeatClase[[x]][valClases[2],dataFungiTest[y,][[x]]]))
+Result_Test2 <- t(Result_Test2)
+Result_Test2_prod <- sapply(seq(1:nrows), function(x) prod(Result_Test2[x,])*Prob_clase[2])
+Result_Test2_prod
+
+
+
 claseEst <- data.frame(e=Result_Test_prod,p=Result_Test2_prod)
+summary(claseEst)
 claseEst$clase <- ifelse(claseEst$p > claseEst$e,'p','e')
 claseEst
 TablaFinal <- data.frame(PpostCe=Result_Test_prod,PpostCp=Result_Test2_prod,claseEst$clase,dataFungiTest$clase)
@@ -114,3 +139,102 @@ Presicion <-  (TP[1,1])/TP[1,1]+TP[2,1]
 
 F_Score <- 2*Presicion*Recall/Presicion*Recall
 
+
+
+#K fold cross 
+dataFungiFtraining <- datafungiTrain
+dataFungiF_training <- datafungiTrain
+#Desordenar datos de trainig-desrodenando indices
+Ndata <- dim(dataFungiFtraining)[1]
+indData <- sample(1:Ndata,Ndata,replace=F)
+#Dividir en k partes, k=10
+
+K <- 10
+ndataFold <- Ndata/K
+indFoldI <- seq(from=1,to=Ndata,by=ndataFold)
+indFold_list <- lapply(indFoldI, function(x) seq(x,x+ndataFold-1))
+fold <- 1
+indTest <- indFold_list[[fold]]
+restoFolds <- seq(2,K)
+#Aqui no se desordenan los datos para ser seleccionado aleatoreamente
+indTraining <- unlist(indFold_list[restoFolds])
+dataTraining <- dataFungiF_training[indTraining,]
+dataTest <- dataFungiF_training[indTest,]
+
+
+#Aqui si
+
+indTest <- indData[indFold_list[[fold]]]
+indTraining <- indData[unlist(indFold_list[restoFolds])]
+dataTraining <- dataFungiF_training[indTraining,]
+dataTest <- dataFungiF_training[indTest,]
+dim(dataTraining)
+dim(dataTest)
+
+
+#APLICACAMOS TODO DE NUEVO
+Exactitud <- c()
+
+Error <-  c()
+
+Recall <-  c()
+
+Especificidad <-  c()
+
+Presicion <-  c()
+
+F_Score <- c()
+
+for (fold in 1:K) {
+  restoFolds <- setdiff(1:K,fold)
+  indTest <- indData[indFold_list[[fold]]]
+  indTraining <- indData[unlist(indFold_list[restoFolds])]
+  dataTraining <- dataFungiF_training[indTraining,]
+  dataTest <- dataFungiF_training[indTest,]
+  
+  verosimiFeatClase <- lapply(namesFeatures, function(x) verIndFC(dataTraining[,1],dataTraining[,x],c("clase",x)))
+  
+  names(verosimiFeatClase) <- namesFeatures
+  
+  Prob_clase<-table(dataTraining$clase)
+  Prob_clase <- Prob_clase/dim(dataTraining)[1]
+  nrows <- nrow(dataTest)
+  Result_Test <- sapply(seq(1:nrows), function(y) sapply(namesFeatures, function(x) verosimiFeatClase[[x]][valClases[1],dataTest[y,][[x]]]))
+  Result_Test <- t(Result_Test)
+  Result_Test_prod <- sapply(seq(1:nrows), function(x) prod(Result_Test[x,])*Prob_clase[1])
+  
+  
+  Result_Test2 <- sapply(seq(1:nrows), function(y) sapply(namesFeatures, function(x) verosimiFeatClase[[x]][valClases[2],dataTest[y,][[x]]]))
+  Result_Test2 <- t(Result_Test2)
+  Result_Test2_prod <- sapply(seq(1:nrows), function(x) prod(Result_Test2[x,])*Prob_clase[2])
+  
+  claseEst <- data.frame(e=Result_Test_prod,p=Result_Test2_prod)
+  claseEst$clase <- ifelse(claseEst$p > claseEst$e,'p','e')
+  dim(claseEst)
+  dim(dataTest)
+  TablaFinal <- data.frame(PpostCe=Result_Test_prod,PpostCp=Result_Test2_prod,claseEst$clase,dataTest$clase)
+  TablaFinal$claseEst.clase <- factor(TablaFinal$claseEst.clase)
+  
+  TP <- table(TablaFinal$claseEst.clase,TablaFinal$dataTest.clase)
+  sumRows_TP<-rowSums(TP)
+  sumCols_TP<-colSums(TP)
+  
+  Exactitud <- c(Exactitud,(TP[1,1]*TP[2,2])/sum(sumRows_TP))
+  
+  Error <-  c(Error,(TP[1,2]*TP[2,1])/sum(sumRows_TP))
+  
+  Recall <-  c(Recall,(TP[1,1])/sumRows_TP[1])
+  
+  Especificidad <-  c(Especificidad,(TP[2,2])/sumRows_TP[2])
+  
+  Presicion <-  c(Presicion,(TP[1,1])/TP[1,1]+TP[2,1])
+  
+  F_Score <- c(F_Score,2*Presicion[fold]*Recall[fold]/Presicion[fold]*Recall[fold])
+  
+}
+mean(Exactitud)
+mean(Error)
+mean(Recall)
+mean(Especificidad)
+mean(Presicion)
+mean(F_Score)
