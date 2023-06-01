2a8f41e2d4381973b3587af27858e60569d6baa2
Will-Mdez <82000613+Will-Mdez@users.noreply.github.com>
2023-05-31 22:33:16 -0600

    Lista Practica K fold Cross Val

--- Bayesiano.R
+++ Bayesiano.R
@@ -1,5 +1,6 @@
-namefile <- "C://Users//alumnos//Documents//GitHub//Pattern-Reconigtion//datasets-20230307T150614Z-001//datasets//agaricus-lepiota.data"
+#namefile <- "C://Users//alumnos//Documents//GitHub//Pattern-Reconigtion//datasets-20230307T150614Z-001//datasets//agaricus-lepiota.data"
 #namefile <-"C://Users//alumnos.SALAC6-05//Documents//Pattern-Reconigtion-main//datasets-20230307T150614Z-001//datasets//agaricus-lepiota.data"
+namefile <- "C://Users//willm//Downloads//1002-A//Metaheuristicas//Pattern-Reconigtion//datasets-20230307T150614Z-001//datasets//agaricus-lepiota.data"
 dataLepiota <- read.table(namefile, header = TRUE, sep = ",")
 colSelec <- c(1,18,20,22,23)
 dataFungi <- dataLepiota[,colSelec]
@@ -185,56 +186,62 @@ Presicion <-  c()
 
 F_Score <- c()
 
+
+
 for (fold in 1:K) {
-  restoFolds <- setdiff(1:K,fold)
+  
   indTest <- indData[indFold_list[[fold]]]
+  restoFolds <- setdiff(1:K, fold)
   indTraining <- indData[unlist(indFold_list[restoFolds])]
   dataTraining <- dataFungiF_training[indTraining,]
   dataTest <- dataFungiF_training[indTest,]
+
+  probClase <- table(dataTraining$clase)
+  probClase <- probClase/dim(dataTraining)[1]
+  probClase
+
+  verosimFeatClase <- lapply(namesFeatures, function(x) verIndFC(dataTraining[,1],dataTraining[,x],c("clase",x)))
+
+  names(verosimFeatClase) <- namesFeatures
+  prob1 <- probClase[1]
   
-  verosimiFeatClase <- lapply(namesFeatures, function(x) verIndFC(dataTraining[,1],dataTraining[,x],c("clase",x)))
   
-  names(verosimiFeatClase) <- namesFeatures
+  valClases <- levels(dataFungiF$clase)
   
-  Prob_clase<-table(dataTraining$clase)
-  Prob_clase <- Prob_clase/dim(dataTraining)[1]
   nrows <- nrow(dataTest)
-  Result_Test <- sapply(seq(1:nrows), function(y) sapply(namesFeatures, function(x) verosimiFeatClase[[x]][valClases[1],dataTest[y,][[x]]]))
-  Result_Test <- t(Result_Test)
-  Result_Test_prod <- sapply(seq(1:nrows), function(x) prod(Result_Test[x,])*Prob_clase[1])
+  probabilidades_test_e <- sapply(seq(1:nrows), function(y) sapply(namesFeatures, function(x) verosimFeatClase[[x]][valClases[1],dataTest[y,][[x]]]))
+  probabilidades_test_e <- t(probabilidades_test_e)
+  Result_Test_prod <- sapply(seq(1:nrows), function(x) prod(probabilidades_test_e[x,])*probClase[1])
+
   
+  Result_Test2_prod <- sapply(seq(1:nrows), function(y) sapply(namesFeatures, function(x) verosimFeatClase[[x]][valClases[2],dataTest[y,][[x]]]))
+  Result_Test2_prod <- t(Result_Test2_prod)
+  proba_p_test <- sapply(seq(1:nrows), function(x) prod(Result_Test2_prod[x,])*probClase[2])
+ 
   
-  Result_Test2 <- sapply(seq(1:nrows), function(y) sapply(namesFeatures, function(x) verosimiFeatClase[[x]][valClases[2],dataTest[y,][[x]]]))
-  Result_Test2 <- t(Result_Test2)
-  Result_Test2_prod <- sapply(seq(1:nrows), function(x) prod(Result_Test2[x,])*Prob_clase[2])
+  TablaFinal <- data.frame(e=Result_Test_prod,p=proba_p_test)
+  TablaFinal$clase_est <- ifelse(TablaFinal$p > TablaFinal$e, 'p', 'e')
+  TablaFinal$clase_real <- dataTest$clase
+  matriz_confusion <- table(TablaFinal$clase_est,dataTest$clase)
+ 
+
+  Exactitud <- c(Exactitud,((matriz_confusion[1]+matriz_confusion[4])/nrows))
   
-  claseEst <- data.frame(e=Result_Test_prod,p=Result_Test2_prod)
-  claseEst$clase <- ifelse(claseEst$p > claseEst$e,'p','e')
-  dim(claseEst)
-  dim(dataTest)
-  TablaFinal <- data.frame(PpostCe=Result_Test_prod,PpostCp=Result_Test2_prod,claseEst$clase,dataTest$clase)
-  TablaFinal$claseEst.clase <- factor(TablaFinal$claseEst.clase)
+  Error <-  c(Error,((matriz_confusion[2]+matriz_confusion[3])/nrows))
   
-  TP <- table(TablaFinal$claseEst.clase,TablaFinal$dataTest.clase)
-  sumRows_TP<-rowSums(TP)
-  sumCols_TP<-colSums(TP)
+  Recall <-  c(Recall,(matriz_confusion[1]/(matriz_confusion[1]+matriz_confusion[3])))
   
-  Exactitud <- c(Exactitud,(TP[1,1]*TP[2,2])/sum(sumRows_TP))
+  Especificidad <-  c(Especificidad,(matriz_confusion[4]/(matriz_confusion[2]+matriz_confusion[4])))
   
-  Error <-  c(Error,(TP[1,2]*TP[2,1])/sum(sumRows_TP))
+  Presicion <-  c(Presicion,(matriz_confusion[1]/(matriz_confusion[1]+matriz_confusion[2])))
   
-  Recall <-  c(Recall,(TP[1,1])/sumRows_TP[1])
-  
-  Especificidad <-  c(Especificidad,(TP[2,2])/sumRows_TP[2])
-  
-  Presicion <-  c(Presicion,(TP[1,1])/TP[1,1]+TP[2,1])
-  
-  F_Score <- c(F_Score,2*Presicion[fold]*Recall[fold]/Presicion[fold]*Recall[fold])
-  
+  F_Score <- c(F_Score,((2*Presicion[length(Presicion)]*Recall[length(Recall)])/(Presicion[length(Presicion)]+Recall[length(Recall)])))
 }
+
 mean(Exactitud)
 mean(Error)
 mean(Recall)
 mean(Especificidad)
 mean(Presicion)
 mean(F_Score)
+
